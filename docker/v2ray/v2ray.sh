6c248ada0a78d225ceb3a7e831a56a07678918db
harveyhuang <harveyhuangw@mynetlink.org>
2026-07-18 17:40:35 +0800

    refactor: update script.

--- docker/v2ray/v2ray.sh
+++ docker/v2ray/v2ray.sh
@@ -1,31 +1,32 @@
 #!/bin/sh
 #
 # This is a Shell script for v2ray based alpine with Docker image
-# 
+#
 # Copyright (C) 2019 - 2022 Teddysun <i@teddysun.com>
 #
 # Reference URL:
 # https://github.com/v2fly/v2ray-core.git
 
 PLATFORM=$1
+
 if [ -z "$PLATFORM" ]; then
-    ARCH="amd64"
+    ARCH="64"
 else
     case "$PLATFORM" in
         linux/386)
-            ARCH="386"
+            ARCH="32"
             ;;
         linux/amd64)
-            ARCH="amd64"
+            ARCH="64"
             ;;
         linux/arm/v6)
-            ARCH="arm6"
+            ARCH="arm32-v6"
             ;;
         linux/arm/v7)
-            ARCH="arm7"
+            ARCH="arm32-v7a"
             ;;
         linux/arm64|linux/arm64/v8)
-            ARCH="arm64"
+            ARCH="arm64-v8a"
             ;;
         linux/ppc64le)
             ARCH="ppc64le"
@@ -38,23 +39,34 @@ else
             ;;
     esac
 fi
+
 [ -z "${ARCH}" ] && echo "Error: Not supported OS Architecture" && exit 1
-# Download binary file
-V2RAY_FILE="v2ray_linux_${ARCH}"
-# V2CTL_FILE="v2ctl_linux_${ARCH}"
 
-echo "Downloading binary file: ${V2RAY_FILE}"
-wget -O /usr/bin/v2ray https://dl.lamp.sh/files/${V2RAY_FILE} > /dev/null 2>&1
+# Download the latest stable release
+V2RAY_FILE="v2ray-linux-${ARCH}.zip"
+V2RAY_URL="https://github.com/v2fly/v2ray-core/releases/latest/download/${V2RAY_FILE}"
+
+echo "Downloading release file: ${V2RAY_FILE}"
+
+wget -O "/tmp/${V2RAY_FILE}" "${V2RAY_URL}" > /dev/null 2>&1
+
 if [ $? -ne 0 ]; then
-    echo "Error: Failed to download binary file: ${V2RAY_FILE}" && exit 1
+    echo "Error: Failed to download release file: ${V2RAY_FILE}"
+    rm -f "/tmp/${V2RAY_FILE}"
+    exit 1
 fi
-echo "Download binary file: ${V2RAY_FILE} completed"
 
-# echo "Downloading binary file: ${V2CTL_FILE}"
-# wget -O /usr/bin/v2ctl https://dl.lamp.sh/files/${V2CTL_FILE} > /dev/null 2>&1
-# if [ $? -ne 0 ]; then
-    # echo "Error: Failed to download binary file: ${V2CTL_FILE}" && exit 1
-# fi
-# echo "Download binary file: ${V2CTL_FILE} completed"
+echo "Download release file: ${V2RAY_FILE} completed"
+
+# Extract binary file
+unzip -p "/tmp/${V2RAY_FILE}" v2ray > /usr/bin/v2ray
+
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to extract binary file: v2ray"
+    rm -f "/tmp/${V2RAY_FILE}" /usr/bin/v2ray
+    exit 1
+fi
+
+rm -f "/tmp/${V2RAY_FILE}"
+
 chmod +x /usr/bin/v2ray
-# chmod +x /usr/bin/v2ctl
--- docker/xray/xray.sh
+++ docker/xray/xray.sh
@@ -1,31 +1,32 @@
 #!/bin/sh
 #
 # This is a Shell script for xray based alpine with Docker image
-# 
+#
 # Copyright (C) 2019 - 2020 Teddysun <i@teddysun.com>
 #
 # Reference URL:
 # https://github.com/XTLS/Xray-core
 
 PLATFORM=$1
+
 if [ -z "$PLATFORM" ]; then
-    ARCH="amd64"
+    ARCH="64"
 else
     case "$PLATFORM" in
         linux/386)
-            ARCH="386"
+            ARCH="32"
             ;;
         linux/amd64)
-            ARCH="amd64"
+            ARCH="64"
             ;;
         linux/arm/v6)
-            ARCH="arm6"
+            ARCH="arm32-v6"
             ;;
         linux/arm/v7)
-            ARCH="arm7"
+            ARCH="arm32-v7a"
             ;;
         linux/arm64|linux/arm64/v8)
-            ARCH="arm64"
+            ARCH="arm64-v8a"
             ;;
         linux/ppc64le)
             ARCH="ppc64le"
@@ -38,15 +39,34 @@ else
             ;;
     esac
 fi
-[ -z "${ARCH}" ] && echo "Error: Not supported OS Architecture" && exit 1
-# Download binary file
-XRAY_FILE="xray_linux_${ARCH}"
 
-echo "Downloading binary file: ${XRAY_FILE}"
-wget -O /usr/bin/xray https://dl.lamp.sh/files/${XRAY_FILE} > /dev/null 2>&1
+[ -z "${ARCH}" ] && echo "Error: Not supported OS Architecture" && exit 1
+
+# Download the latest stable release
+XRAY_FILE="Xray-linux-${ARCH}.zip"
+XRAY_URL="https://github.com/XTLS/Xray-core/releases/latest/download/${XRAY_FILE}"
+
+echo "Downloading release file: ${XRAY_FILE}"
+
+wget -O "/tmp/${XRAY_FILE}" "${XRAY_URL}" > /dev/null 2>&1
+
 if [ $? -ne 0 ]; then
-    echo "Error: Failed to download binary file: ${XRAY_FILE}" && exit 1
+    echo "Error: Failed to download release file: ${XRAY_FILE}"
+    rm -f "/tmp/${XRAY_FILE}"
+    exit 1
 fi
-echo "Download binary file: ${XRAY_FILE} completed"
+
+echo "Download release file: ${XRAY_FILE} completed"
+
+# Extract binary file
+unzip -p "/tmp/${XRAY_FILE}" xray > /usr/bin/xray
+
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to extract binary file: xray"
+    rm -f "/tmp/${XRAY_FILE}" /usr/bin/xray
+    exit 1
+fi
+
+rm -f "/tmp/${XRAY_FILE}"
 
 chmod +x /usr/bin/xray
